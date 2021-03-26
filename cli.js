caf5f746172f7867a1c9a05e76c07cb2f2c99ca4
KillDozerX2 <himanshupant127@gmail.com>
2021-03-27 03:04:44 +0530

    feat: :sparkles: Added feature to accept output directory
    This will allow a user to pass a output directory as a argument

--- cli.js
+++ cli.js
@@ -2,7 +2,12 @@ const path = require('path');
 const cpy = require('cpy');
 
 (async () => {
-	await cpy('starter', path.join(process.cwd(), 'documentation'),
+    let destination = process.argv.length >= 2 ? process.argv[2] : 'documentation';
+    console.log(destination);
+    if (!path.isAbsolute(destination)) {
+        destination = path.join(process.cwd(), destination);
+    }
+	await cpy('starter', path.join(destination),
         {
             cwd: __dirname,
             dot: true
